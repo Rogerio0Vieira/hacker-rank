af2236f6e6d880823f51214c08d976aa030e095f
Rogério Vieira <rogeriovbh@gmail.com>
2025-09-06 02:39:11 +0000

    feat:  implements compare triples method

---
+++ compare-triples.cs
@@ -0,0 +1,60 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        List<int> c = new List<int> {0,0};
+        
+        for(int i = 0; i < a.Count ; i++){
+            if(a[i] > b[i]){
+                 if(a[i] == 0 && b[i] == 0){
+                    c[1] = c[1] + 1;
+                 }else{
+                     c[0] = c[0] + 1;
+                 }
+            }
+            else if(a[i] < b[i]){
+                 if(a[i] == 0 && b[i] == 0){
+                    c[1] = c[1] + 1;
+                 }else{
+                     c[1] = c[1] + 1;
+                 }
+            }
+        }
+        return c;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = Result.compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
